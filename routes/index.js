a0d5cf74259a9ac3518bdfd187d1674817b01b2b
StefanMitrikeski <mi34ence94@gmail.com>
2019-12-24 14:46:44 +0100

    Added Sequelize and MySQL

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+let index = require('../controllers/index');
 
+/* GET home page. */
+router.get('/', index.index);
 module.exports = router;
