8ce1ad1efd1bff477c3b91eef2f5d638fb41030a
StefanMitrikeski <mi34ence94@gmail.com>
2019-12-25 13:42:24 +0100

    user and login form

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
+const models = require("../models");
 exports.index=function(req, res, next) {
     res.render('index', { title: 'Express' });
   };
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 let index = require('../controllers/index');
+let user = require('../controllers/user');
+
+router.get('/login', user.show_login);
+router.get('/signup', user.show_signup);
+router.post('/login', user.login);
+router.post('/signup', user.signup);
 
 /* GET home page. */
 router.get('/', index.index);
